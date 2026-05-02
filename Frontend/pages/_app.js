89c36a64b6567fc7787c69b1c41f828b962b30d3
TMnyenyembe <bsc-com-ne-02-21@unima.ac.mw>
2026-05-02 11:19:48 +0200

    core app configuration and global styles

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,10 +1,21 @@
 // pages/_app.js
 import "../styles/globals.css";
 import 'leaflet/dist/leaflet.css';
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "600", "700"] });
+import Head from "next/head";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </div>
+  );
 }
